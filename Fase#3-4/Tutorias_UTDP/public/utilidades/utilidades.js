51a2ff4de4b0aec768d7aecd8b8de04213c4ded5
Emilianovich <emiolafagbemi02@gmail.com>
2025-11-01 09:46:57 -0500

    Modificando estilos generales y utilidades

--- Fase#3-4/Tutorias_UTDP/public/utilidades/utilidades.js
+++ Fase#3-4/Tutorias_UTDP/public/utilidades/utilidades.js
@@ -1,3 +1,20 @@
+const opcionesNav = document.querySelectorAll("div.opciones-nav");
+const opcionSeleccionada = document.getElementById("opcion_nav-seleccionada");
+
+opcionesNav.forEach((opcion) => {
+    opcion.addEventListener("mouseover", ()=> {
+        if (opcion.id !== "opcion_nav-seleccionada") {
+            opcion.style.cursor = "pointer";
+            opcionSeleccionada.classList.remove("nav-selected");
+        }
+    })
+    opcion.addEventListener("mouseout", ()=> {
+        if (opcion.id !== "opcion_nav-seleccionada") {
+            opcionSeleccionada.classList.add("nav-selected");
+        }
+    })
+})
+
 function cerrarSesion (avatar) {
     avatar.style.cursor = "pointer";
     avatar.addEventListener("click", ()=> {
