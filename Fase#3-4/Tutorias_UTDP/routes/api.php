ebdf87d7a7b960d206b53c60dcc44bbec73e9a9b
Emilianovich <emiolafagbemi02@gmail.com>
2025-11-01 17:44:30 -0500

    Creación de todos los controladores (para la Fase#4)

--- Fase#3-4/Tutorias_UTDP/routes/api.php
+++ Fase#3-4/Tutorias_UTDP/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\SesionController;
+use App\Http\Controllers\ImparteController;
+use App\Http\Controllers\InscripcionController;
+use App\Http\Controllers\EvaluacionController;
+
 
 Route::get('/bienvenida', function () {
     return ["mensaje" => "Bienvenidos a Tutorías UTDP"];
