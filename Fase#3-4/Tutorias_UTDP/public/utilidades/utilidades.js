1364c3362fd82df52ca6ee77b9541a422e2f9be2
Emilianovich <emiolafagbemi02@gmail.com>
2025-11-04 23:11:52 -0500

    Arreglando el bug del menú deslizable

--- Fase#3-4/Tutorias_UTDP/public/utilidades/utilidades.js
+++ Fase#3-4/Tutorias_UTDP/public/utilidades/utilidades.js
@@ -1,76 +1,108 @@
+/*Nav dinámico*/
 const opcionesNav = document.querySelectorAll("div.opciones-nav");
 const opcionSeleccionada = document.getElementById("opcion_nav-seleccionada");
-const botonCerrar = document.getElementById("botonCerrar");
-const slidingMenu = document.querySelector(".nav-celular");
-//slidingMenu.style.display = "none";
-
-/*Nav dinámico*/
 opcionesNav.forEach((opcion) => {
-    opcion.addEventListener("mouseover", ()=> {
+    opcion.addEventListener("mouseover", () => {
         if (opcion.id !== "opcion_nav-seleccionada") {
             opcion.style.cursor = "pointer";
             opcionSeleccionada.classList.remove("nav-selected");
         }
     })
-    opcion.addEventListener("mouseout", ()=> {
+    opcion.addEventListener("mouseout", () => {
         if (opcion.id !== "opcion_nav-seleccionada") {
             opcionSeleccionada.classList.add("nav-selected");
         }
     })
 })
 
+/*Creación del menú deslizable*/
+const slidingMenu = document.createElement("section");
+slidingMenu.classList.add("nav-celular");
+
+const contenedorMenu = document.createElement("article");
+contenedorMenu.id = "nav-celular-opciones";
+
+contenedorBotonCerrar = document.createElement("article");
+contenedorBotonCerrar.id = "contenedor-boton-cerrar";
+
+const botonCerrar = document.createElement("img");
+botonCerrar.id = "botonCerrar";
+botonCerrar.src = "../images/close.png";
+contenedorBotonCerrar.appendChild(botonCerrar);
+
+const opcionInicio = document.createElement("p");
+const linkInicio = document.createElement("a");
+linkInicio.innerText = "Inicio";
+linkInicio.href = "";
+opcionInicio.appendChild(linkInicio);
+
+const opcionMateria = document.createElement("p");
+const linkMateria = document.createElement("a");
+linkMateria.innerText = "Materia";
+linkMateria.href = "materias.html";
+opcionMateria.appendChild(linkMateria);
+
+const opcionEvaluar = document.createElement("p");
+const linkEvaluar = document.createElement("a");
+linkEvaluar.innerText = "Evaluar";
+linkEvaluar.href = "";
+opcionEvaluar.appendChild(linkEvaluar);
+
+const opcionNosotros = document.createElement("p");
+const linkNosotros = document.createElement("a");
+linkNosotros.innerText = "Nosotros";
+linkNosotros.href = "";
+opcionNosotros.appendChild(linkNosotros);
+
+contenedorMenu.append(contenedorBotonCerrar, opcionInicio, opcionMateria, opcionEvaluar, opcionNosotros);
+slidingMenu.appendChild(contenedorMenu);
+
 /*Agregando hamburger menu al header*/
 const screenSize = window.matchMedia("(max-width: 480px)");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const hamburgerMenu = document.createElement("img");
-hamburgerMenu.src="../images/hamburger_menu.png";
+hamburgerMenu.src = "../images/hamburger_menu.png";
 hamburgerMenu.id = "hamburger-menu";
 hamburgerMenu.alt = "Hamburger menu by Md Tanvirul Haque";
 const iconoPerfil = document.getElementById("picture-icono-perfil");
 const contenedorHamburgerPerfil = document.createElement("div");
 contenedorHamburgerPerfil.id = "contenedorHamburgerPerfil";
-function addHamburgerMenu (event) {
+
+function addHamburgerMenu(event) {
     if (event.matches) {
         addHamburgerAccount();
-    }
-    else {
+    } else {
         removeHamburgerAccount();
-        slidingMenu.style.display = "none";
     }
 }
 
-function addHamburgerAccount () {
-    document.body.removeChild(nav);
+function addHamburgerAccount() {
+    nav?.remove();
     contenedorHamburgerPerfil.appendChild(hamburgerMenu);
     contenedorHamburgerPerfil.appendChild(iconoPerfil);
     header.appendChild(contenedorHamburgerPerfil);
 }
-function removeHamburgerAccount () {
+
+function removeHamburgerAccount() {
     document.body.appendChild(nav);
-    header.removeChild(contenedorHamburgerPerfil);
+    contenedorHamburgerPerfil?.remove();
     header.appendChild(iconoPerfil);
 }
+
 screenSize.addEventListener("change", addHamburgerMenu);
 addHamburgerMenu(screenSize);
 
-function cerrarSesion (avatar) {
-    avatar.style.cursor = "pointer";
-    avatar.addEventListener("click", ()=> {
-        sessionStorage.clear();
-        window.location.href("http://127.0.0.1:63342/Login/login.html");
-    })
-}
-
 /*Mostrando el hamburger menu al usuario*/
-slidingMenu.addEventListener("mouseover", ()=> {
+slidingMenu.addEventListener("mouseover", () => {
     slidingMenu.cursor = "default";
 })
-hamburgerMenu.addEventListener("click", ()=> {
+hamburgerMenu.addEventListener("click", () => {
+    document.body.append(slidingMenu);
     slidingMenu.style.display = "block";
     slidingMenu.classList.add("mostrar-hamburger");
 });
-botonCerrar.addEventListener("click", ()=> {
+botonCerrar.addEventListener("click", () => {
     slidingMenu.classList.remove("mostrar-hamburger");
     slidingMenu.classList.add("quitar-hamburger");
 })
@@ -131,4 +163,13 @@ function mostrarPopUpSinRedirect(mensaje, src) {
     contenedorPopUp.append(popUp);
     document.body.append(contenedorPopUp);
     contenedorPopUp.classList.add("mostrar");
+}
+
+/*Revisar cuando esté listo el login*/
+function cerrarSesion(avatar) {
+    avatar.style.cursor = "pointer";
+    avatar.addEventListener("click", () => {
+        sessionStorage.clear();
+        window.location.href("http://127.0.0.1:63342/Login/login.html");
+    })
 }
