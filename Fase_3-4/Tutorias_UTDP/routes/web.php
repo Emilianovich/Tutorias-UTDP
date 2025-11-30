14641d67fd331327c8d620124cfd6a96a292ea69
Emilianovich <emiolafagbemi02@gmail.com>
2025-11-30 18:27:56 -0500

    Quitando el debugging de web.php

--- Fase_3-4/Tutorias_UTDP/routes/web.php
+++ Fase_3-4/Tutorias_UTDP/routes/web.php
@@ -6,12 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/debug-env', function () {
-    return [
-        'DB_HOST' => env('DB_HOST'),
-        'DB_PORT' => env('DB_PORT'),
-        'DB_DATABASE' => env('DB_DATABASE'),
-        'DB_USERNAME' => env('DB_USERNAME'),
-    ];
-});
-
