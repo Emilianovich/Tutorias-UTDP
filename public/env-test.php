caa553f542b03e6d78239d43d41abaf9b824dea2
Emilianovich <emiolafagbemi02@gmail.com>
2025-12-04 01:13:50 -0500

    Borrando public/env-test.php

--- public/env-test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<pre>";
-echo "DB_HOST: " . getenv('DB_HOST') . "\n";
-echo "DB_PORT: " . getenv('DB_PORT') . "\n";
-echo "DB_DATABASE: " . getenv('DB_DATABASE') . "\n";
-echo "DB_USERNAME: " . getenv('DB_USERNAME') . "\n";
-echo "</pre>";
